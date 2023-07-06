d56ca10dae8780ecede1cb250c8856cbe1696d46
Yannick <reckey55@fu-berlin.de>
2023-07-06 16:34:44 +0200

    Create table with df in postgres

---
+++ convertDateCreatePostgres.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import psycopg2
+import io
+from config import config
+from sqlalchemy import create_engine
+
+with open(("src/Fahrraddiebstahl.csv"), encoding="latin-1") as test:
+    fahrraddiebstahl=pd.read_csv(test)
+
+# Merge TATZEIT_ANFANG_DATUM + TATZEIT_ANFANG_STUNDE and TATZEIT_ENDE_DATUM + TATZEIT_ENDE_STUNDE 
+# Change datatype to timestamp
+# Delete column TATZEIT_ANFANG_STUNDE and TATZEIT_ENDE_STUNDE 
+def StringtoDate(df, column1, column2):
+    for i in range(len(df[column1])):
+        df[column1][i] = pd.to_datetime((df[column1][i] + " " + str(df[column2][i]) + ":00"), format= '%d.%m.%Y %H:%M')
+    del df[column2]
+
+# Change VERSUCH column to booleans 
+def VersuchToBool(df):
+    map= {"Ja":True, "Nein": False}
+    return df.replace({"VERSUCH":map})
+
+StringtoDate(fahrraddiebstahl, "TATZEIT_ANFANG_DATUM", "TATZEIT_ANFANG_STUNDE")
+StringtoDate(fahrraddiebstahl, "TATZEIT_ENDE_DATUM", "TATZEIT_ENDE_STUNDE")
+fahrraddiebstahl=VersuchToBool(fahrraddiebstahl)
+
+
+# Create table in postgres with dataframe 
+engine = create_engine(
+    'postgresql+psycopg2://pm:admindb@localhost:5432/bikes')
+
+fahrraddiebstahl.head(0).to_sql('table_name', engine, if_exists='replace',index=False)
+
+params = config('database.ini')
+conn = engine.raw_connection()
+cur = conn.cursor()
+output = io.StringIO()
+fahrraddiebstahl.to_csv(output, sep='\t', header=False, index=False)
+output.seek(0)
+contents = output.getvalue()
+cur.copy_from(output, 'table_name', null="") # null values become ''
+conn.commit()
+cur.close()
+conn.close()
