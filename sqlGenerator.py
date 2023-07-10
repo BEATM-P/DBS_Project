5c4a4c4c93419e61d7a3c7d478616c514c2bb744
BEATM_P <pesimei@gmail.com>
2023-07-10 16:08:31 +0200

    update handler can handle Bezirke and Fahrradart

--- sqlGenerator.py
+++ sqlGenerator.py
@@ -3,8 +3,23 @@ class SQLGenerator:
         self.sqls={"select": ['"PLR_ID"','"PLR_NAME"','"Gemeinde_name"'],
                 "from": ['"lor_pl"', '"fahrraddiebstahl"', '"bezirksgrenzen"'],
                 "joins":['"LOR" = "PLR_ID"', '"Gemeinde_schluessel" = "BEZ"'],
-                "cond": {"bezirke":[], "types":[], "showVersuch":True, "showVersuch":None, "starttime":None, "endtime": None}}
+                "cond": {"bezirke":set(), "types":set(), "showVersuch":True, "ShowSuccess":True, "starttime":None, "endtime": None}}
         
+
+    def update_handler(self, Bezirk,ArtdesFahrrads, Tageszeit, Versuch):
+        self.sqls["cond"]["bezirke"]=set(Bezirk)
+
+        self.sqls["cond"]["types"]=set(ArtdesFahrrads)
+
+        self.versuch("Versuchter Diebstahl"in Versuch, "Erfolgreicher Diebstahl" in Versuch)        
+        
+        self.tageszeit("Tag"in Tageszeit, "Nacht"in Tageszeit)
+
+        return self.construct_sql()
+     
+
+
+
     def construct_sql(self):
         strq=""
         strq+=("SELECT ")
@@ -13,13 +28,23 @@ class SQLGenerator:
         strq+=' , '.join(self.sqls["from"])
         strq+=("\nWHERE ")
         strq+='\n AND '.join(self.sqls["joins"])
-        for i in self.sqls["cond"]["bezirke"]:
-           strq+=f'\n AND "Gemeinde_name"= {i}'
-        for i in self.sqls["cond"]["types"]:
-           strq+=f'\n AND "ART_DES_FAHRRADS"= {i}'
+        if self.sqls["cond"]["bezirke"]!=set():
+            strq+="\n AND "
+            for i in self.sqls["cond"]["bezirke"]:
+                strq+=f'"Gemeinde_name"= \'{i}\' OR'
+            strq+=f'"Gemeinde_name"= \'DUMMY VALUE\''
+        if self.sqls["cond"]["types"]!=set():
+            strq+="\n AND "
+            for i in self.sqls["cond"]["types"]:
+                strq+=f'"ART_DES_FAHRRADS"= \'{i}\' OR'
+            strq+=f'"ART_DES_FAHRRADS"= \'DUMMY VALUE\''
+
         print(strq)
         return strq
 
+
+    def tageszeit(self, tag=None, nacht=None):
+        pass
 
     def add_time(self, starttime=None, endtime=None):
         if starttime==None and endtime==None:
@@ -50,6 +75,7 @@ class SQLGenerator:
             self.sqls.cond["types"].append(type)
 
     def versuch(self, showVersuch=None, showSuccess=None):
+        print(showSuccess,showVersuch)
         if showVersuch==None and showSuccess==None:
             print("Who called this function without input?")
             return
@@ -58,7 +84,8 @@ class SQLGenerator:
         elif showSuccess!=None:
             self.sqls["cond"]["showSuccess"]=showSuccess
 
+
 if __name__=="__main__":
     print("UNIT TESTING SQL GENERATION OR SMTH")
     G=SQLGenerator()
-    G.construct_sql()
+    G.update_handler(['Mitte'],['Fahrrad'], ['Tag', 'Nacht'],['Versuchter Diebstahl'])
